81f8b0e3cb11e9c3e38c3e36a658222ce1f59712
massacrach <s.kalisty.job@gmail.com>
2021-10-20 22:49:30 +0300

    initial commit

---
+++ config-server/src/test/java/my/app/microservices/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.app.microservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
